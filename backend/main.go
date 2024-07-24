4123c614d0682834d4a647784cfb925eca35a898
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-24 22:28:28 +0600

    execute connection

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/OlyMahmudMugdho/gotenv/gotenv"
 	"github.com/OlyMahmudMugdho/url-shortener/utils"
 )
 
 func main() {
 	gotenv.Load()
-	utils.ConnectToDatabase()
+	if utils.ConnectToDatabase() {
+		fmt.Println("connected to the database")
+	}
 }
