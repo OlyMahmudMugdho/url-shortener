d33b131f1c154f42061035bd8b6077506247e415
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-24 22:24:56 +0600

    connect to database

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/OlyMahmudMugdho/gotenv/gotenv"
+import (
+	"github.com/OlyMahmudMugdho/gotenv/gotenv"
+	"github.com/OlyMahmudMugdho/url-shortener/utils"
+)
 
 func main() {
 	gotenv.Load()
+	utils.ConnectToDatabase()
 }
