8d5c96330aaa34c3cf22d26f79c4ba99bdaf6e08
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-24 22:42:29 +0600

    type User

--- backend/types/types.go
+++ backend/types/types.go
@@ -6,3 +6,12 @@ type PostgresConfig struct {
 	Db       string
 	Sslmode  string
 }
+
+type User struct {
+	Id        string `json:id`
+	Name      string `json: "name"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Password  string `json:"password"`
+	Email     string `json:"email"`
+}
