2dafbcb653bb163f6003b46876105de61e543141
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-24 22:55:13 +0600

    SaveUser method

--- backend/services/auth/store.go
+++ backend/services/auth/store.go
@@ -1,6 +1,12 @@
 package auth
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/OlyMahmudMugdho/url-shortener/types"
+)
 
 type AuthStore struct {
 	db *sql.DB
@@ -10,4 +16,17 @@ func NewAuthStore(db *sql.DB) *AuthStore {
 	return &AuthStore{
 		db: db,
 	}
+}
+
+func (a *AuthHandler) SaveUser(user types.UserRequestBody) error {
+
+	var query string = fmt.Sprintf(`INSERT INTO users (USERNAME, PASSWORD, FIRST_NAME, LAST_NAME, EMAIL) VALUES ('%s','%s','%s','%s','%s')`, user.Username, user.Password, user.FirstName, user.LastName, user.LastName)
+
+	_, error := a.store.db.Exec(query)
+
+	if error != nil {
+		log.Fatal(error)
+		return error
+	}
+	return nil
 }
