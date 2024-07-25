a2449cee877ee2629de4f592c704f701cc2bedd7
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-25 11:29:21 +0600

    Register user method

--- backend/services/auth/routes.go
+++ backend/services/auth/routes.go
@@ -1,5 +1,12 @@
 package auth
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/OlyMahmudMugdho/url-shortener/types"
+)
+
 type AuthHandler struct {
 	store *AuthStore
 }
@@ -7,5 +14,25 @@ type AuthHandler struct {
 func NewAuthHandler(store *AuthStore) *AuthHandler {
 	return &AuthHandler{
 		store: store,
+	}
+}
+
+func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request, userBody types.UserRequestBody) any {
+	user := new(types.User)
+
+	user.Username = userBody.Username
+	user.Password = userBody.Password
+	user.Email = userBody.Email
+	user.FirstName = userBody.FirstName
+	user.LastName = userBody.LastName
+
+	error := h.SaveUser(*user)
+
+	if error != nil {
+		log.Fatal(error)
+		w.WriteHeader(403)
+		return nil
+	} else {
+		return userBody
 	}
 }
