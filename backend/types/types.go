ac9938d0b70cbc26c4e1432c5f106799a850e366
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-26 12:42:03 +0600

    removed User related structs to User Model

--- backend/types/types.go
+++ backend/types/types.go
@@ -6,27 +6,3 @@ type PostgresConfig struct {
 	Db       string
 	Sslmode  string
 }
-
-type User struct {
-	Id        string `json:"id"`
-	Username  string `json:"username"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Password  string `json:"password"`
-	Email     string `json:"email"`
-}
-
-type UserRequestBody struct {
-	Username  string `json:"username"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Password  string `json:"password"`
-	Email     string `json:"email"`
-}
-
-type UserResponseBody struct {
-	Username  string `json:"username"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-}
