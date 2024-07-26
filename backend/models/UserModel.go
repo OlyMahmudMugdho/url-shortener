4d577804e054fa6ea11c298bc24aeb36fd865c19
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-26 12:41:20 +0600

    added User Model

---
+++ backend/models/UserModel.go
@@ -0,0 +1,25 @@
+package models
+
+type User struct {
+	Id        string `json:"id"`
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Password  string `json:"password"`
+	Email     string `json:"email"`
+}
+
+type UserRequestBody struct {
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Password  string `json:"password"`
+	Email     string `json:"email"`
+}
+
+type UserResponseBody struct {
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+}
