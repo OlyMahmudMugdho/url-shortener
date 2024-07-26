f168aee821b76647088c284091326cb830d0a900
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-26 17:09:38 +0600

    FindUserByUserName

--- backend/services/auth/store.go
+++ backend/services/auth/store.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"database/sql"
 	"fmt"
+
 	"github.com/OlyMahmudMugdho/url-shortener/models"
 )
 
@@ -16,14 +17,27 @@ func NewAuthStore(db *sql.DB) *AuthStore {
 	}
 }
 
-func (h *AuthHandler) SaveUser(user models.User) error {
+func (h *AuthStore) SaveUser(user models.User) error {
 
 	var query string = fmt.Sprintf(`INSERT INTO users (USERNAME, PASSWORD, FIRST_NAME, LAST_NAME, EMAIL) VALUES ('%s','%s','%s','%s','%s')`, user.Username, user.Password, user.FirstName, user.LastName, user.Email)
 
-	_, error := h.store.db.Exec(query)
+	_, error := h.db.Exec(query)
 
 	if error != nil {
 		return error
 	}
 	return nil
+}
+
+func (h *AuthStore) FindUserByUserName(username string) (models.User, error) {
+	user := new(models.User)
+	var query string = `SELECT * FROM "users" WHERE USERNAME=$1`
+	row := h.db.QueryRow(query, username)
+	err := row.Scan(&user.Id, &user.Username, &user.Password, &user.FirstName, &user.LastName, &user.Email)
+
+	if err != nil {
+		return *user, err
+	}
+
+	return *user, nil
 }
