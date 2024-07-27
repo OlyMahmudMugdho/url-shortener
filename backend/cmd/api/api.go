06efa9423e969ae76ebd1f308f9193321c4209b6
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-27 13:26:20 +0600

    experimental dev endpoint

--- backend/cmd/api/api.go
+++ backend/cmd/api/api.go
@@ -2,11 +2,14 @@ package api
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/OlyMahmudMugdho/gotenv/gotenv"
+	"github.com/OlyMahmudMugdho/url-shortener/middlewares"
 	"github.com/OlyMahmudMugdho/url-shortener/services/auth"
+	"github.com/OlyMahmudMugdho/url-shortener/types"
 	"github.com/OlyMahmudMugdho/url-shortener/utils"
 )
 
@@ -42,6 +45,7 @@ func (h *ApiServer) Run() {
 
 	authStore := auth.NewAuthStore(h.db)
 	authHandler := auth.NewAuthHandler(authStore)
+	h.router.Handle("GET /dev", middlewares.VerifyAuthentication(Hello))
 	authHandler.RegisterRoutes(h.router)
 
 	log.Printf("server is listening on port %v", h.port)
@@ -50,4 +54,11 @@ func (h *ApiServer) Run() {
 	if error != nil {
 		log.Fatal(error)
 	}
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	var k types.ContextKey = "username"
+	ctx := r.Context().Value(k)
+	fmt.Println(ctx)
+	w.Write([]byte(ctx.(string)))
 }
