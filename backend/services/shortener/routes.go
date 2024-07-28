5047f4b7dc9f85c6ab1567af62cdf2ce8060c836
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-28 20:43:38 +0600

    add url handler

--- backend/services/shortener/routes.go
+++ backend/services/shortener/routes.go
@@ -1,5 +1,11 @@
 package shortener
 
+import (
+	"encoding/json"
+	"github.com/OlyMahmudMugdho/url-shortener/models"
+	"net/http"
+)
+
 type Handler struct {
 	store *Store
 }
@@ -7,5 +13,26 @@ type Handler struct {
 func NewShortenerHandler(store *Store) *Handler {
 	return &Handler{
 		store: store,
+	}
+}
+
+func (h *Handler) AddUrl(w http.ResponseWriter, r *http.Response) {
+	link := new(models.Link)
+
+	err := json.NewDecoder(r.Body).Decode(&link)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	savedLink, err := h.store.SaveLink(link)
+	if err != nil {
+		return
+	}
+
+	w.WriteHeader(200)
+	err = json.NewEncoder(w).Encode(savedLink)
+	if err != nil {
+		return
 	}
 }
