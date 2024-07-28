6a37d8e74c7ef546c083959859d2d261aa9cefa6
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-29 01:52:09 +0600

    return columns updated; error fix

--- backend/services/shortener/store.go
+++ backend/services/shortener/store.go
@@ -18,9 +18,9 @@ func NewShortenerStore(db *sql.DB) *Store {
 }
 
 func (s *Store) SaveLink(link *models.Link) (*models.Link, error) {
-	var query = `INSERT INTO "urls" (user_id, full_url, short_url, created_at) VALUES ($1,$2,$3,$4) RETURNING url_id, user_id, full_url, short_url, updated_at, created_at`
+	var query = `INSERT INTO "urls" (user_id, full_url, short_url, created_at) VALUES ($1,$2,$3,$4) RETURNING url_id, user_id, full_url, short_url, created_at`
 	row := s.db.QueryRow(query, link.UserId, link.FullUrl, link.ShortUrl, link.CreatedAt)
-	err := row.Scan(&link.Id, &link.UserId, &link.FullUrl, &link.ShortUrl, &link.UpdatedAt, &link.CreatedAt)
+	err := row.Scan(&link.Id, &link.UserId, &link.FullUrl, &link.ShortUrl, &link.CreatedAt)
 
 	if err != nil {
 		log.Println(err)
