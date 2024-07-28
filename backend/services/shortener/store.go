695ae25fc6ad1f9b4cfba2c509532428c8e8a7e5
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-28 20:43:14 +0600

    Save a Link (url)

--- backend/services/shortener/store.go
+++ backend/services/shortener/store.go
@@ -1,6 +1,9 @@
 package shortener
 
-import "database/sql"
+import (
+	"database/sql"
+	"github.com/OlyMahmudMugdho/url-shortener/models"
+)
 
 type Store struct {
 	db *sql.DB
@@ -10,4 +13,16 @@ func NewShortenerStore(db *sql.DB) *Store {
 	return &Store{
 		db: db,
 	}
+}
+
+func (s *Store) SaveLink(link *models.Link) (*models.Link, error) {
+	var query = `INSERT INTO "urls" (user_id, full_url, short_url, updated_at, created_at) VALUES ($1,$2,$3,$4,$5)`
+	row := s.db.QueryRow(query, link.UserId, link.FullUrl, link.ShortUrl, link.UpdatedAt, link.CreatedAt)
+	err := row.Scan(&link.Id, &link.UserId, &link.FullUrl, &link.ShortUrl, &link.UpdatedAt, &link.CreatedAt)
+
+	if err != nil {
+		return link, err
+	}
+
+	return link, nil
 }
