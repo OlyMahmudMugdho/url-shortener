c389d0f9a3ad5d277059d335e9b03d62d38fa0cc
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-29 01:50:21 +0600

    updated query in savlink fn

--- backend/services/shortener/store.go
+++ backend/services/shortener/store.go
@@ -2,8 +2,9 @@ package shortener
 
 import (
 	"database/sql"
-	"github.com/OlyMahmudMugdho/url-shortener/models"
 	"log"
+
+	"github.com/OlyMahmudMugdho/url-shortener/models"
 )
 
 type Store struct {
@@ -17,8 +18,8 @@ func NewShortenerStore(db *sql.DB) *Store {
 }
 
 func (s *Store) SaveLink(link *models.Link) (*models.Link, error) {
-	var query = `INSERT INTO "urls" (user_id, full_url, short_url, updated_at, created_at) VALUES ($1,$2,$3,$4,$5)`
-	row := s.db.QueryRow(query, link.UserId, link.FullUrl, link.ShortUrl, link.UpdatedAt, link.CreatedAt)
+	var query = `INSERT INTO "urls" (user_id, full_url, short_url, created_at) VALUES ($1,$2,$3,$4) RETURNING url_id, user_id, full_url, short_url, updated_at, created_at`
+	row := s.db.QueryRow(query, link.UserId, link.FullUrl, link.ShortUrl, link.CreatedAt)
 	err := row.Scan(&link.Id, &link.UserId, &link.FullUrl, &link.ShortUrl, &link.UpdatedAt, &link.CreatedAt)
 
 	if err != nil {
