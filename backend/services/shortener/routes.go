3427083dba29a2724758424a466e4910ee77a83b
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-29 16:29:46 +0600

    delete method

--- backend/services/shortener/routes.go
+++ backend/services/shortener/routes.go
@@ -28,6 +28,7 @@ func (h *Handler) RegisterRoutes(router *http.ServeMux) {
 	router.Handle("POST /add-url", middlewares.VerifyAuthentication(h.AddUrl))
 	router.Handle("GET /links", middlewares.VerifyAuthentication(h.GetAllLinks))
 	router.Handle("GET /links/{urlId}", middlewares.VerifyAuthentication(h.GetLink))
+	router.Handle("DELETE /links/{urlId}", middlewares.VerifyAuthentication(h.DeleteLink))
 }
 
 func (h *Handler) AddUrl(w http.ResponseWriter, r *http.Request) {
@@ -103,4 +104,39 @@ func (h *Handler) GetLink(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(500)
 		return
 	}
+}
+
+func (h *Handler) DeleteLink(w http.ResponseWriter, r *http.Request) {
+	context := r.Context()
+	userId := utils.GetUserIdFromContext(context)
+
+	urlIdStr, ok := utils.ExtractParamFromUrl(r.URL.Path, "/links/")
+	if !ok {
+		w.WriteHeader(400)
+		return
+	}
+
+	urlId, err := strconv.Atoi(urlIdStr)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(400)
+		return
+	}
+
+	err = h.store.DeleteLink(urlId, userId)
+
+	if err != nil {
+		log.Println(err)
+		_ = json.NewEncoder(w).Encode(map[string]any{
+			"error":   true,
+			"message": "something went wrong",
+		})
+		return
+	}
+
+	_ = json.NewEncoder(w).Encode(map[string]any{
+		"ok":      true,
+		"message": "deleted",
+	})
+
 }
