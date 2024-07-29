ff254a5c9547fc01b9b9141452d33855944b0d68
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-29 12:48:45 +0600

    updated Link Model

--- backend/models/LinkModel.go
+++ backend/models/LinkModel.go
@@ -4,7 +4,7 @@ import "time"
 
 type Link struct {
 	Id        int       `json:"id"`
-	UserId    int       `json:"userId"`
+	UserId    string    `json:"userId"`
 	FullUrl   string    `json:"fullUrl"`
 	ShortUrl  string    `json:"shortUrl"`
 	UpdatedAt time.Time `json:"updatedAt"`
