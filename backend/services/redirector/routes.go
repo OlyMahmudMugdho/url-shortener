4e61f8398f600a89deb2a452de04f1d1568afbba
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-29 18:04:57 +0600

    Redirect method

--- backend/services/redirector/routes.go
+++ backend/services/redirector/routes.go
@@ -1,6 +1,13 @@
 package redirector
 
-import "github.com/OlyMahmudMugdho/url-shortener/services/shortener"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/OlyMahmudMugdho/url-shortener/services/shortener"
+)
 
 type Handler struct {
 	shortenerStore *shortener.Store
@@ -10,4 +17,24 @@ func NewRedirectorHandler(store *shortener.Store) *Handler {
 	return &Handler{
 		shortenerStore: store,
 	}
+}
+
+func (h *Handler) RegisterRoutes(router *http.ServeMux) {
+	router.HandleFunc("GET /", h.Redirect)
+}
+
+func (h *Handler) Redirect(w http.ResponseWriter, r *http.Request) {
+	url := r.RequestURI
+	shotURl, _ := strings.CutPrefix(url, "/")
+
+	link, err := h.shortenerStore.GetPublicLink(shotURl)
+
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(404)
+		return
+	}
+
+	fmt.Println(link.FullUrl)
+	http.Redirect(w, r, link.FullUrl, http.StatusSeeOther)
 }
