991c015a0742a83ae19a929026c64e9d978de409
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-31 12:36:11 +0600

    imports and styles

--- frontend/url-shortener-frontend/src/app/app.component.ts
+++ frontend/url-shortener-frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {MenuComponent} from "./menu/menu.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, MenuComponent],
+  imports: [RouterOutlet, HomeComponent, MenuComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
