78055b389ca5e144d0385cf2ca2f78a85f1cbee3
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-31 10:24:04 +0600

    openlink method updated

--- backend/services/redirector/routes.go
+++ backend/services/redirector/routes.go
@@ -2,11 +2,10 @@ package redirector
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"strings"
-
 	"github.com/OlyMahmudMugdho/url-shortener/services/shortener"
+	"net/http"
+	"os"
+	"strings"
 )
 
 type Handler struct {
@@ -20,21 +19,33 @@ func NewRedirectorHandler(store *shortener.Store) *Handler {
 }
 
 func (h *Handler) RegisterRoutes(router *http.ServeMux) {
-	router.HandleFunc("GET /", h.Redirect)
+	// Remove the HTTP method from the route definition
+	router.HandleFunc("GET /", h.OpenLink)
 }
 
-func (h *Handler) Redirect(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) OpenLink(w http.ResponseWriter, r *http.Request) {
 	url := r.RequestURI
 	shotURl, _ := strings.CutPrefix(url, "/")
 
 	link, err := h.shortenerStore.GetPublicLink(shotURl)
 
 	if err != nil {
-		log.Println(err)
+		w.Header().Add("Content-Type", "text/html")
 		w.WriteHeader(404)
+		data, _ := os.ReadFile("views/404.html")
+		w.Write(data)
 		return
 	}
 
+	if err != nil {
+		w.WriteHeader(404)
+		data, _ := os.ReadFile("views/404.html")
+		_, err := w.Write(data)
+		if err != nil {
+			return
+		}
+		return
+	}
 	fmt.Println(link.FullUrl)
 	http.Redirect(w, r, link.FullUrl, http.StatusSeeOther)
 }
