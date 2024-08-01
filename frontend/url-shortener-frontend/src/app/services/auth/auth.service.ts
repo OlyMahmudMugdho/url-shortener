2c1647deb40a056c523b462b9fe96b28dac87f13
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-08-01 13:52:38 +0600

    added auth service

---
+++ frontend/url-shortener-frontend/src/app/services/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn : 'root'
+})
+
+
+
+export class AuthService {
+
+  private isAuthenticated :boolean = false
+  private url :string = "http://localhost:8080/login"
+  private httpOptions :any = {
+    header : new HttpHeaders({
+      "Content-Type" : "application/json"
+    }),
+    withCredentials : true
+  }
+
+  constructor(private httpClient : HttpClient) { }
+  sendLoginData(data :any) : Observable<any>{
+    return this.httpClient.post(this.url, JSON.stringify(data), this.httpOptions)
+  }
+
+  setAuthenticated(value : boolean) {
+    this.isAuthenticated = value;
+    alert(this.isAuthenticated)
+  }
+}
